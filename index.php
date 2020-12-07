a2c530d04c1b87348e7f7e3a3851e47a849c661b
Jenifer Prochnow <jenifer.prochnow@the-white-label.com>
2020-12-07 09:14:14 +0100

    Added Totos

--- index.php
+++ index.php
@@ -13,3 +13,5 @@ echo $parser->parseTemplate();
 // TODO: Add Projects
 // TODO: Add own contentpage for each project
 // TODO: Make slider dynamic to have mor than 3 projects
+// TODO: Project Icons Größe anpassen
+// TODO? Mobil Bewegung der Icons bei Bewegung des Devices?
