2a9b88e85355b6563e7a773f5d4286841849cefc
Jenifer Prochnow <jenifer.prochnow@the-white-label.com>
2020-12-09 20:29:19 +0100

    Implemented async loading

--- index.php
+++ index.php
@@ -3,14 +3,22 @@
 include_once 'Translator.php';
 include_once 'TemplateParser.php';
 
+
+if (!empty($_GET['ajax'] && !empty($_GET['template']))) {
+    $parser = new TemplateParser('components/inc_' . $_GET['template'] .  '.html');
+    $parser->setVariable('rootUrl', 'http://localhost/jencoding');
+
+    if ($_GET['template'] === 'contact') {
+        $parser->setVariable('contactMail', 'moin@jencoding.com');
+
+    }
+    echo $parser->parseTemplate();
+    die();
+}
+
 $parser = new TemplateParser('template.html');
 $parser->setVariable('rootUrl', 'https://jencoding.com');
-$parser->setVariable('contactMail', 'moin@jencoding.com');
 
 echo $parser->parseTemplate();
 
-// TODO: Add Projects
-// TODO: Add own contentpage for each project
-// TODO: Make slider dynamic to have mor than 3 projects
-// TODO: Project Icons Größe anpassen
-// TODO? Mobil Bewegung der Icons bei Bewegung des Devices?
+// TODO: Add Project Content
